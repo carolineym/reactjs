53b28b7fe407b2d68e7b46d0982deabcf241e085
Caroline Ming <carolineming1101@gmail.com>
2018-12-23 18:48:57 -0500

    practising

--- my-app/src/components/counters.jsx
+++ my-app/src/components/counters.jsx
@@ -10,12 +10,46 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[0] = { ...counter };
+    counters[0].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleDelete = counterId => {
+    // console.log("Event Handler Called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           //value1 corresponds to the value1 in counter.jsx
-          <Counter key={counter.id} value1={counter.value} id={counter.id} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            // value1={counter.value}
+            // id={counter.id}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          />
         ))}
       </div>
     );
